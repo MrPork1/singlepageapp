afbbd8242af2d4c6cf4aaf95c92183e0192aaec8
Sean <seankel@comcast.net>
2022-05-19 22:11:24 -0700

    Added styling for better mobile viewing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Dropdown, InputGroup, FormControl, FormLabel, ButtonGroup, Form} from 'react-bootstrap';
+import {FormLabel, Form} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { text } from '@fortawesome/fontawesome-svg-core';
 
 
 function App() {
@@ -79,37 +77,67 @@ function App() {
 
       <h3>Sean's Beer Searching</h3>
 
-      <div className="searchArea">
-      <FormLabel htmlFor="beerSearch">Search API for Beers</FormLabel>
+          <div className="searchArea">
+                <div class="d-none d-lg-block">
+                  <FormLabel htmlFor="beerSearch">Search API for Beers</FormLabel>
 
-      <div class="input-group mb-3">
-        <input ref={textInput} type="text" class="form-control" id="search" placeholder="Search for beer..." name="search"></input>
-        <button type="submit" class="btn btn-primary" onClick={searchItems}><FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon></button>
+
+                <div class="input-group mb-3">
+                  <input ref={textInput} type="text" class="form-control" id="search" placeholder="Search for beer..." name="search"></input>
+                  <button type="submit" class="btn btn-primary" onClick={searchItems}><FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon></button>
+                </div>
+
+
+            <Form.Label>Filter By</Form.Label>
+            <Form.Select onChange={(e) => filterResultsFromDropdown(e.target.value)}>
+                <option value=""></option>
+                <option value="first_brewed">First brewed</option>
+                <option value="ph">ph level</option>
+              </Form.Select>
+              </div>
+
+              <div class=".d-none d-md-block d-lg-none">
+                <form>
+                  <div class="row">
+                    <div class="col">
+                <FormLabel htmlFor="beerSearch">Search API for Beers</FormLabel>
+
+
+                <div class="input-group mb-3">
+                  <input ref={textInput} type="text" class="form-control" id="search" placeholder="Search for beer..." name="search"></input>
+                  <button type="submit" class="btn btn-primary" onClick={searchItems}><FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon></button>
+                </div>
+                    </div>
+                    <div class="col">
+                <Form.Label>Filter By</Form.Label>
+                  <Form.Select onChange={(e) => filterResultsFromDropdown(e.target.value)}>
+                    <option value=""></option>
+                    <option value="first_brewed">First brewed</option>
+                    <option value="ph">ph level</option>
+                  </Form.Select>
+                    </div>
+                  </div>
+                </form>
+              </div>
+
+
+          </div>
+
       </div>
-
-      <Form.Label>Filter By</Form.Label>
-      <Form.Select onChange={(e) => filterResultsFromDropdown(e.target.value)}>
-          <option value=""></option>
-          <option value="first_brewed">First brewed</option>
-          <option value="ph">ph level</option>
-        </Form.Select>
-        </div>
-
-        </div>
 
       <div className="App-body">
         <div className="resultsSection">
         {isError == true ? (
-          <h4>Failed to get data from the API.</h4>
+          <h5>Failed to get data from the API.</h5>
         ) : (
           <p></p>
           )}
 
 
           {searchInput.length > 0 ? (
-            <h4>{filteredResults.length > 0 ? <h4>Number of results from search: {filteredResults.length}</h4> : 'No results found!'}</h4>
+            <h5>{filteredResults.length > 0 ? <h5>Number of results from search: {filteredResults.length}</h5> : 'No results found!'}</h5>
           ) : (
-            <h4>Number of results: {beers.length}</h4>
+            <h5>Number of results: {beers.length}</h5>
           )}
           </div>
 
